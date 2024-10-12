d3ad74c539c5e735358bfc25547804caf2684dae
Nighty3098 <154594695+Nighty3098@users.noreply.github.com>
2024-10-12 17:30:03 +0700

    Update log_monitor.c

--- src/log_monitor.c
+++ src/log_monitor.c
@@ -26,22 +26,18 @@ long int warning_count = 0;
 long int info_count = 0;
 long int debug_count = 0;
 
-void handle_signal(int signal)
-{
-  if (signal == SIGINT)
-  {
+void handle_signal(int signal) {
+  if (signal == SIGINT) {
     running = 0;
     printf("\nExiting ...\n");
   }
 }
 
-int compile_regex(regex_t *regex, const char *pattern)
-{
+int compile_regex(regex_t *regex, const char *pattern) {
   return regcomp(regex, pattern, REG_EXTENDED | REG_ICASE);
 }
 
-void *count_log_levels(void *arg)
-{
+void *count_log_levels(void *arg) {
   const char *line = (const char *)arg;
 
   regex_t regex;
@@ -50,13 +46,10 @@ void *count_log_levels(void *arg)
 
   pthread_mutex_lock(&count_mutex);
 
-  for (int i = 0; i < 4; i++)
-  {
+  for (int i = 0; i < 4; i++) {
     if (compile_regex(&regex, patterns[i]) == 0 &&
-        regexec(&regex, line, 0, NULL, 0) == 0)
-    {
-      switch (i)
-      {
+        regexec(&regex, line, 0, NULL, 0) == 0) {
+      switch (i) {
       case 0:
         critical_count++;
         break; // CRITICAL
@@ -80,21 +73,16 @@ void *count_log_levels(void *arg)
   return NULL;
 }
 
-void process_lines(char *buffer, const char *filter_level, FILE *output_file)
-{
+void process_lines(char *buffer, const char *filter_level, FILE *output_file) {
   char *line = strtok(buffer, "\n");
-  while (line != NULL)
-  {
-    if (should_print_log(line, filter_level))
-    {
-      if (output_file != NULL)
-      {
+  while (line != NULL) {
+    if (should_print_log(line, filter_level)) {
+      if (output_file != NULL) {
         fprintf(output_file, "%s\n", line);
-      }
-      else
-      {
+      } else {
         colorize_log(line);
-        // printf("%s\n", line); // Print to console only if no file is specified
+        // printf("%s\n", line); // Print to console only if no file is
+        // specified
       }
 
       count_log_levels(line);
@@ -103,34 +91,30 @@ void process_lines(char *buffer, const char *filter_level, FILE *output_file)
   }
 }
 
-void print_statistics(FILE *output_file)
-{
-  if (output_file != NULL)
-  {
+void print_statistics(FILE *output_file, int real_time) {
+  if (output_file != NULL) {
     fprintf(output_file, "\nLog Statistics:\n");
     fprintf(output_file, "CRITICAL: %ld\n", critical_count);
     fprintf(output_file, "WARNING: %ld\n", warning_count);
     fprintf(output_file, "INFO: %ld\n", info_count);
     fprintf(output_file, "DEBUG: %ld\n", debug_count);
-  }
-  else
-  {
-    printf("\nLog Statistics:\n");
-    printf("CRITICAL: %ld\n", critical_count);
-    printf("WARNING: %ld\n", warning_count);
-    printf("INFO: %ld\n", info_count);
-    printf("DEBUG: %ld\n", debug_count);
+  } else {
+    if (!real_time) {
+      printf("\nLog Statistics:\n");
+      printf("CRITICAL: %ld\n", critical_count);
+      printf("WARNING: %ld\n", warning_count);
+      printf("INFO: %ld\n", info_count);
+      printf("DEBUG: %ld\n", debug_count);
+    }
   }
 }
 
 void start_log_monitor(const char *file_name, const char *filter_level,
-                       int real_time, const char *output_file_name)
-{
+                       int real_time, const char *output_file_name) {
   signal(SIGINT, handle_signal);
 
   int fd = open(file_name, O_RDONLY);
-  if (fd == -1)
-  {
+  if (fd == -1) {
     perror("open");
     return;
   }
@@ -138,29 +122,25 @@ void start_log_monitor(const char *file_name, const char *filter_level,
   char buffer[BUFFER_SIZE];
   FILE *output_file = NULL;
 
-  if (output_file_name)
-  {
+  if (output_file_name) {
     output_file = fopen(output_file_name, "w");
-    if (!output_file)
-    {
+    if (!output_file) {
       perror("fopen");
       close(fd);
       return;
     }
   }
 
-  if (!real_time)
-  {
+  if (!real_time) {
     ssize_t bytes_read;
-    while ((bytes_read = read(fd, buffer, sizeof(buffer) - 1)) > 0)
-    {
+    while ((bytes_read = read(fd, buffer, sizeof(buffer) - 1)) > 0) {
       buffer[bytes_read] = '\0';
       process_lines(buffer, filter_level, output_file);
     }
 
     close(fd);
 
-    print_statistics(output_file);
+    print_statistics(output_file, real_time);
 
     if (output_file != NULL)
       fclose(output_file);
