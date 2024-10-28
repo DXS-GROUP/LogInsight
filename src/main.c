146fd1e06c318452ea6d474f698c0867fe659e22
Nighty3098 <154594695+Nighty3098@users.noreply.github.com>
2024-10-28 23:07:32 +0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,8 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void print_usage(const char *program_name)
-{
+void print_usage(const char *program_name) {
   printf("\n");
   printf("\n\033[0;32m");
   printf("▄▄▌         ▄▄ • ▪   ▐ ▄ .▄▄ · ▪   ▄▄ •  ▄ .▄▄▄▄▄▄\n");
@@ -15,42 +14,32 @@ void print_usage(const char *program_name)
   printf(".▀▀▀  ▀█▄▀▪·▀▀▀▀ ▀▀▀▀▀ █▪ ▀▀▀▀ ▀▀▀·▀▀▀▀ ▀▀▀ · ▀▀▀ \n");
   printf("\n");
   printf("\n\033[1;33m");
-  printf("Usage: %s [-r] [-f <level>] -i <file> [-o <output>]\n", program_name);
+  printf("Usage: %s [-r] [-f <level>] -i <file>\n", program_name);
   printf("  -r             Display all changes in real time\n");
   printf("  -f <level>     Level filtering (CRITICAL, WARNING, INFO, DEBUG)\n");
   printf("  -i <file>      Path to log file\n");
   printf("  -h, --help     Show this help\n\033[0m");
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   const char *file_name = NULL;
   const char *filter_level = NULL;
   int real_time = 0;
 
-  for (int i = 1; i < argc; i++)
-  {
-    if (strcmp(argv[i], "-i") == 0 && i + 1 < argc)
-    {
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-i") == 0 && i + 1 < argc) {
       file_name = argv[++i];
-    }
-    else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
-    {
+    } else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
       print_usage(argv[0]);
       return EXIT_SUCCESS;
-    }
-    else if (strcmp(argv[i], "-f") == 0 && i + 1 < argc)
-    {
+    } else if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) {
       filter_level = argv[++i];
-    }
-    else if (strcmp(argv[i], "-r") == 0)
-    {
+    } else if (strcmp(argv[i], "-r") == 0) {
       real_time = 1;
     }
   }
 
-  if (!file_name)
-  {
+  if (!file_name) {
     print_usage(argv[0]);
     return EXIT_FAILURE;
   }
