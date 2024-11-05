4151ec678867edbf19203bcd02015949554f2540
Nighty3098 <154594695+Nighty3098@users.noreply.github.com>
2024-11-05 21:42:11 +0700

    🌊 Working on new functional

--- src/log_analyzer.py
+++ src/log_analyzer.py
@@ -1,10 +1,13 @@
 import os
 import platform
+import time
 from datetime import datetime
 from collections import defaultdict
 from file_reader import get_file_info, read_lines
 from log_filter import filter_by_level, filter_by_datetime
 from colorizer import Colorizer
+from prettytable import PrettyTable
+from prettytable import MARKDOWN
 
 class LogAnalyzer:
     def __init__(self, file_path, levels=None, date_range=None):
@@ -14,6 +17,7 @@ class LogAnalyzer:
         self.lines_count = 0
         self.colorizer = Colorizer()
         self.level_counts = defaultdict(int)
+        self.isRealTime = False
 
     def format_file_size(self, size):
         """Format file size into appropriate units."""
@@ -65,7 +69,7 @@ class LogAnalyzer:
             level = self.extract_log_level(line)
             self.level_counts[level] += 1
 
-    def output_statistics(self, lines):
+    def output_statistics(self, lines, isRealTime):
         """Output statistics about the analyzed log."""
         self.lines_count = len(lines)
         self.count_levels(lines)
@@ -75,27 +79,57 @@ class LogAnalyzer:
             colored_line = self.colorizer.colorize(line.strip(), level)
             print(colored_line)
 
-        print("\n\033[0;32m┌─────────────────────────────")
-        for level, count in self.level_counts.items():
-            if count > 0:
-                print(f"│ {level.capitalize()}: {count}")
-        print("└─────────────────────────────\033[0m")
+        if (isRealTime != True):
+            level_table = PrettyTable()
+            level_table.set_style(MARKDOWN)
 
-        print("\n\033[0;34m┌─────────────────────────────")
-        print(f"│ Lines read: {self.lines_count}")
+            level_table.border = True
+            level_table.header = False
+            level_table.padding_width = 5
+            level_table.vertical_char = "│"
+            level_table.horizontal = "─"
+            level_table._horizontal_align_char = ""
+            level_table.junction_char = "│"
 
-        file_info = get_file_info(self.file_path)
-        formatted_size = self.format_file_size(file_info['size'])
-        print(f"│ File size: {formatted_size}")
+            level_table.field_names = ["Log Level", "Count"]
+            for level, count in self.level_counts.items():
+                if count > 0:
+                    level_table.add_row([level.capitalize(), count])
+            
+            print("\n\n\033[0;32m│ ⬤  STATISTIC: \n│")
+            print(f"\033[0;32m{level_table}\033[0m\n")
 
-        creation_date, modification_date = self.get_file_dates()
-        if modification_date:
-            print(f"│ Last modification date: {modification_date}")
+            file_table = PrettyTable()
+            file_table.set_style(MARKDOWN)
 
-        print("\033[0;34m└─────────────────────────────")
+            file_table.border = True
+            file_table.header = False
+            file_table.padding_width = 5
+            file_table.vertical_char = "│"
+            file_table.horizontal = "─"
+            file_table._horizontal_align_char = ""
+            file_table.junction_char = "│"
+
+            file_table.field_names = ["Parameter", "Value"]
+            file_table.add_row(["Lines read", self.lines_count])
+
+            file_info = get_file_info(self.file_path)
+            formatted_size = self.format_file_size(file_info['size'])
+            file_table.add_row(["File size", formatted_size])
+
+            creation_date, modification_date = self.get_file_dates()
+            if modification_date:
+                file_table.add_row(["Last modification date", modification_date])
+            
+            print("\033[0;34m│ ⬤  INFO: \n│")
+            print(f"\033[0;34m{file_table}\033[0m\n")
+        else:
+            pass
 
     def analyze(self):
         """Analyze the log file and output statistics."""
+        
+        isRealTime = False
         lines = read_lines(self.file_path)
 
         if self.date_range:
@@ -104,4 +138,30 @@ class LogAnalyzer:
         if self.levels:
             lines = filter_by_level(lines, self.levels)
 
-        self.output_statistics(lines)
+        self.output_statistics(lines, isRealTime)
+
+
+    def analyze_real_time(self):
+        """Analyze the log file in real-time and output only new lines."""
+        
+        previous_lines_count = len(read_lines(self.file_path))
+        isRealTime = True
+
+        while True:
+            time.sleep(0.0000001)
+
+            current_lines = read_lines(self.file_path)
+            current_lines_count = len(current_lines)
+
+            if current_lines_count > previous_lines_count:
+                new_lines = current_lines[previous_lines_count:]
+
+                if self.date_range:
+                    new_lines = filter_by_datetime(new_lines, self.date_range)
+
+                if self.levels:
+                    new_lines = filter_by_level(new_lines, self.levels)
+
+                self.output_statistics(new_lines, isRealTime)
+                
+                previous_lines_count = current_lines_count
